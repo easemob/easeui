7a2bf43f7d7b0423ae2939a565fa49e3b752b2fd
linan <greenmemo@163.com>
2016-01-22 10:34:00 +0800

    fix video read ack

--- src/com/hyphenate/easeui/widget/chatrow/EaseChatRowVideo.java
+++ src/com/hyphenate/easeui/widget/chatrow/EaseChatRowVideo.java
@@ -111,7 +111,6 @@ public class EaseChatRowVideo extends EaseChatRowFile{
         intent.putExtra("remotepath", videoBody.getRemoteUrl());
         if (message != null && message.direct() == EMMessage.Direct.RECEIVE && !message.isAcked()
                 && message.getChatType() == ChatType.Chat) {
-            message.setAcked(true);
             try {
                 EMClient.getInstance().chatManager().ackMessageRead(message.getFrom(), message.getMsgId());
             } catch (Exception e) {
