ac7c439a84c0b13ab5bedb7326cfd7ef2652d5e1
zhanw <258653667@qq.com>
2016-03-21 16:33:17 +0800

    add setConversationListeHelper api to conversationList

--- src/com/easemob/easeui/adapter/EaseConversationAdapater.java
+++ src/com/easemob/easeui/adapter/EaseConversationAdapater.java
@@ -4,14 +4,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.content.Context;
-import android.text.Spannable;
-import android.text.SpannableString;
 import android.text.TextUtils;
-import android.text.style.BackgroundColorSpan;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
