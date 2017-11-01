6fad12c79e3af28719a555698169f675c0a160ef
lzan13 <lzan13.gg@gmail.com>
2017-11-01 17:20:15 +0800

    解决第一次进入 app 时一直显示连接不到服务器的问题

--- easeui/src/main/java/com/hyphenate/easeui/ui/EaseConversationListFragment.java
+++ easeui/src/main/java/com/hyphenate/easeui/ui/EaseConversationListFragment.java
@@ -78,6 +78,7 @@ public class EaseConversationListFragment extends EaseBaseFragment{
         // button to clear content in search bar
         clearSearch = (ImageButton) getView().findViewById(R.id.search_clear);
         errorItemContainer = (FrameLayout) getView().findViewById(R.id.fl_error_item);
+        errorItemContainer.setVisibility(View.GONE);
     }
     
     @Override
