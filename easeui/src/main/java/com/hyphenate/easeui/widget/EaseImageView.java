9a52e399fa6d9545635b82c7b53639d31282df2e
zhaoliang <zhaoliang@easemob.com>
2018-12-25 15:55:56 +0800

    use api 28.

--- easeui/src/main/java/com/hyphenate/easeui/widget/EaseImageView.java
+++ easeui/src/main/java/com/hyphenate/easeui/widget/EaseImageView.java
@@ -129,13 +129,8 @@ public class EaseImageView extends ImageView {
         paint.setColor(0xffffffff);
         paint.setAntiAlias(true); //smooths out the edges of what is being drawn
         PorterDuffXfermode xfermode = new PorterDuffXfermode(PorterDuff.Mode.SRC_IN);
-        // set flags
-        int saveFlags = Canvas.MATRIX_SAVE_FLAG
-                | Canvas.CLIP_SAVE_FLAG
-                | Canvas.HAS_ALPHA_LAYER_SAVE_FLAG
-                | Canvas.FULL_COLOR_LAYER_SAVE_FLAG
-                | Canvas.CLIP_TO_LAYER_SAVE_FLAG;
-        canvas.saveLayer(0, 0, width, height, null, saveFlags);
+
+        canvas.saveLayer(0, 0, width, height, null);
 
         if (shapeType == 1) {
             canvas.drawCircle(width / 2, height / 2, width / 2 - 1, paint);
