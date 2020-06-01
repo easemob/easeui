673c982d4d418e92bceebe5dd276e325500d529c
neil <neil.liu@easemob.com>
2020-06-01 11:52:07 +0800

    add custom message type for message list.

---
+++ easeui/src/main/java/com/hyphenate/easeui/widget/chatrow/EaseChatRowCustom.java
@@ -0,0 +1,112 @@
+package com.hyphenate.easeui.widget.chatrow;
+
+import android.content.Context;
+import android.text.Spannable;
+import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.TextView.BufferType;
+
+import com.hyphenate.chat.EMCustomMessageBody;
+import com.hyphenate.chat.EMMessage;
+import com.hyphenate.chat.EMTextMessageBody;
+import com.hyphenate.easeui.R;
+import com.hyphenate.easeui.model.EaseDingMessageHelper;
+import com.hyphenate.easeui.utils.EaseSmileUtils;
+
+import java.util.List;
+
+public class EaseChatRowCustom extends EaseChatRow{
+
+	private TextView contentView;
+
+    public EaseChatRowCustom(Context context, EMMessage message, int position, BaseAdapter adapter) {
+		super(context, message, position, adapter);
+	}
+
+	@Override
+	protected void onInflateView() {
+		inflater.inflate(message.direct() == EMMessage.Direct.RECEIVE ?
+				R.layout.ease_row_received_message : R.layout.ease_row_sent_message, this);
+	}
+
+	@Override
+	protected void onFindViewById() {
+		contentView = (TextView) findViewById(R.id.tv_chatcontent);
+	}
+
+    @Override
+    public void onSetUpView() {
+        EMCustomMessageBody txtBody = (EMCustomMessageBody) message.getBody();
+        String msg = context.getString(R.string.custom_message, txtBody.event());
+        contentView.setText(msg);
+    }
+
+    @Override
+    protected void onViewUpdate(EMMessage msg) {
+        switch (msg.status()) {
+            case CREATE:
+                onMessageCreate();
+                break;
+            case SUCCESS:
+                onMessageSuccess();
+                break;
+            case FAIL:
+                onMessageError();
+                break;
+            case INPROGRESS:
+                onMessageInProgress();
+                break;
+        }
+    }
+
+    public void onAckUserUpdate(final int count) {
+        if (ackedView != null) {
+            ackedView.post(new Runnable() {
+                @Override
+                public void run() {
+                    ackedView.setVisibility(VISIBLE);
+                    ackedView.setText(String.format(getContext().getString(R.string.group_ack_read_count), count));
+                }
+            });
+        }
+    }
+
+    private void onMessageCreate() {
+        progressBar.setVisibility(View.VISIBLE);
+        statusView.setVisibility(View.GONE);
+    }
+
+    private void onMessageSuccess() {
+        progressBar.setVisibility(View.GONE);
+        statusView.setVisibility(View.GONE);
+
+        // Show "1 Read" if this msg is a ding-type msg.
+        if (EaseDingMessageHelper.get().isDingMessage(message) && ackedView != null) {
+            ackedView.setVisibility(VISIBLE);
+            int count = message.groupAckCount();
+            ackedView.setText(String.format(getContext().getString(R.string.group_ack_read_count), count));
+        }
+
+        // Set ack-user list change listener.
+        EaseDingMessageHelper.get().setUserUpdateListener(message, userUpdateListener);
+    }
+
+    private void onMessageError() {
+        progressBar.setVisibility(View.GONE);
+        statusView.setVisibility(View.VISIBLE);
+    }
+
+    private void onMessageInProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+        statusView.setVisibility(View.GONE);
+    }
+
+    private EaseDingMessageHelper.IAckUserUpdateListener userUpdateListener =
+            new EaseDingMessageHelper.IAckUserUpdateListener() {
+                @Override
+                public void onUpdate(List<String> list) {
+                    onAckUserUpdate(list.size());
+                }
+            };
+}
