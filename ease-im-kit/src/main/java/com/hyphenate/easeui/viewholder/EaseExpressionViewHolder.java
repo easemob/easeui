e8e0f927b65540e6045658fe11f413e5d3a5b177
liujinan <liujinan@edreamtree.com>
2021-01-21 15:26:46 +0800

    add send read ack logic for big expression.

--- ease-im-kit/src/main/java/com/hyphenate/easeui/viewholder/EaseExpressionViewHolder.java
+++ ease-im-kit/src/main/java/com/hyphenate/easeui/viewholder/EaseExpressionViewHolder.java
@@ -5,6 +5,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 
+import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMMessage;
 import com.hyphenate.easeui.adapter.EaseBaseRecyclerViewAdapter;
 import com.hyphenate.easeui.adapter.EaseMessageAdapter;
@@ -12,6 +13,7 @@ import com.hyphenate.easeui.interfaces.MessageListItemClickListener;
 import com.hyphenate.easeui.model.styles.EaseMessageListItemStyle;
 import com.hyphenate.easeui.widget.chatrow.EaseChatRow;
 import com.hyphenate.easeui.widget.chatrow.EaseChatRowBigExpression;
+import com.hyphenate.exceptions.HyphenateException;
 
 public class EaseExpressionViewHolder extends EaseChatRowViewHolder {
 
@@ -24,4 +26,15 @@ public class EaseExpressionViewHolder extends EaseChatRowViewHolder {
         return new EaseExpressionViewHolder(new EaseChatRowBigExpression(parent.getContext(), isSender), itemClickListener);
     }
 
+    @Override
+    protected void handleReceiveMessage(EMMessage message) {
+        super.handleReceiveMessage(message);
+        if (!message.isAcked() && message.getChatType() == EMMessage.ChatType.Chat) {
+            try {
+                EMClient.getInstance().chatManager().ackMessageRead(message.getFrom(), message.getMsgId());
+            } catch (HyphenateException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
