10decf526c4b7755bb5592a1b6c389d72ba567f6
neil <neil.liu@easemob.com>
2021-03-29 16:32:49 +0800

    Hyperlinks are supported in text messages.

--- ease-im-kit/src/main/java/com/hyphenate/easeui/widget/chatrow/EaseChatRowText.java
+++ ease-im-kit/src/main/java/com/hyphenate/easeui/widget/chatrow/EaseChatRowText.java
@@ -1,7 +1,13 @@
 package com.hyphenate.easeui.widget.chatrow;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.text.Html;
 import android.text.Spannable;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.text.style.URLSpan;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.TextView.BufferType;
@@ -37,9 +43,47 @@ public class EaseChatRowText extends EaseChatRow {
     @Override
     public void onSetUpView() {
         EMTextMessageBody txtBody = (EMTextMessageBody) message.getBody();
-        Spannable span = EaseSmileUtils.getSmiledText(context, txtBody.getMessage());
+        //解析html超链接
+        String content = txtBody.getMessage().replace("\n", "<br />");
+        //fromHtml method will ignore \n in string
+        CharSequence htmpTxt;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            htmpTxt = Html.fromHtml(content.replace("<", "&lt;"), Html.FROM_HTML_MODE_LEGACY);
+        } else {
+            htmpTxt = Html.fromHtml(content.replace("<", "&lt;"));
+        }
+
+        String new_content = htmpTxt.toString().replace("<br />", "\n");
+        //解析表情
+        Spannable span = EaseSmileUtils.getSmiledText(context, new_content);
+
+        //给超链接添加响应
+        URLSpan[] urlSpans = span.getSpans(0, htmpTxt.length(), URLSpan.class);
+        for (URLSpan span1 : urlSpans) {
+            int start = span.getSpanStart(span1);
+            int end = span.getSpanEnd(span1);
+            int flag = span.getSpanFlags(span1);
+            final String link = span1.getURL();
+            span.setSpan(new ClickableSpan() {
+                @Override
+                public void onClick(View widget) {
+                    //打开超链接
+                    if (link != null && link.startsWith("http")) {
+                        Intent intent = new Intent();
+                        intent.setAction("android.intent.action.VIEW");
+                        Uri content_url = Uri.parse(link);
+                        intent.setData(content_url);
+                        getContext().startActivity(intent);
+                    }
+                }
+            }, start, end, flag);
+            span.removeSpan(span1);
+        }
+
+        contentView.setLinksClickable(true);
+        contentView.setMovementMethod(LinkMovementMethod.getInstance());
         // 设置内容
-        contentView.setText(span, BufferType.SPANNABLE);
+        contentView.setText(span, TextView.BufferType.SPANNABLE);
     }
 
     @Override
